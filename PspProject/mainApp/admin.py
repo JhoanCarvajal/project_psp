17739fa13c04d018ab6a79b87227bf0c7b28b739
Jhoan Sebastian Carvajal Quinchia <jhoans127@outlook.es>
2020-06-24 21:28:40 -0500

    modelo de lengaje y de medida añadidos, se esta enpezando a hacer el link de cada proyecto

--- PspProject/mainApp/admin.py
+++ PspProject/mainApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Lenguaje, Medida
 
 # Register your models here.
+class LenguajeAdmin(admin.ModelAdmin):
+    list_display= ('nombre',)
+admin.site.register(Lenguaje,LenguajeAdmin)
+
+class MedidaAdmin(admin.ModelAdmin):
+    list_display= ('nombre',)
+admin.site.register(Medida,MedidaAdmin)
--- PspProject/mainApp/models.py
+++ PspProject/mainApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Lenguaje(models.Model):
+    nombre=models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name ="lenguaje"
+        verbose_name_plural ="lenguajes"
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class Medida(models.Model):
+    nombre=models.CharField(max_length=50)
+    descripcion=models.TextField()
+
+    class Meta:
+        verbose_name ="Medida de tamaño"
+        verbose_name_plural ="Medidas de tamaño"
+    
+    def __str__(self):
+        return self.nombre
+
+    
