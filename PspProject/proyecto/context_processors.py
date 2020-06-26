542f791490a8af06781f1f15f17c5984fef16c0e
Michael <michass1425@gmail.com>
2020-06-26 17:23:03 -0500

    se establcieron vistas de los proyectos

--- PspProject/proyecto/context_processors.py
+++ PspProject/proyecto/context_processors.py
@@ -2,6 +2,7 @@ from proyecto.models import Proyect
 
 def get_proyecto(request):
     proyecto= Proyect.objects.values_list('id','nombre','fase' ,'descripcion', 'fechaInicio')
+    
     return {
         'proyectos':proyecto
     }
