ed91ae7672db76cc58dd675e971601603867623c
Michael <michass1425@gmail.com>
2020-07-26 16:38:51 -0500

    programas

--- PspProject/programas/views.py
+++ PspProject/programas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url='login')
+def programa(request):
+    return render(request,'programaT/programa.html',{
+        'title':'Programa'
+    })
