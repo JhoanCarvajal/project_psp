bbd6ac703a02ed2419a2679c46f074c448faea66
Michael <michass1425@gmail.com>
2020-07-30 11:22:28 -0500

    asdf

--- PspProject/reportes/urls.py
+++ PspProject/reportes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import ReporteListView
+from .views import ReporteListView, ReporteCreateView
 
 
 urlpatterns = [
     path('lista/', ReporteListView.as_view(), name='lista_reportes'),
-    
+    path('create/', ReporteCreateView.as_view(), name='create_reportes'), 
 ]
--- PspProject/reportes/views.py
+++ PspProject/reportes/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Reporte
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
 
 
 class ReporteListView(ListView):
     model = Reporte
+
+class ReporteCreateView(CreateView):
+    model = Reporte
+    fields = ['id_programa','nombre','objetivos','condiciones','resultados_esperados','numero_test','resultados_actules','descripcion']
+    success_url = reverse_lazy('lista_reportes')
+
+    
+                          
+                            
+                           
+                 
     
