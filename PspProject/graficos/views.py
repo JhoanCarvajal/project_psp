797dca50e966f5b4d27f89848bcaec8356c003d7
Jhoan Sebastian Carvajal Quinchia <jhoans127@outlook.es>
2020-08-01 15:44:32 -0500

    casi full

--- PspProject/graficos/views.py
+++ PspProject/graficos/views.py
@@ -22,15 +22,42 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.base import TemplateView
 #funciones
-from .funciones.resumen_tiempos import matrizResumenTiempos
-from .funciones.defectos_en_fase import defectos_en_fase
-from .funciones.defectos_removidos_fase import defectos_removidos_fase
+from programas.funciones.resumen_tiempos import matrizResumenTiempos
+from programas.funciones.defectos_en_fase import defectos_en_fase
+from programas.funciones.defectos_removidos_fase import defectos_removidos_fase
 #requerir login
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
 
 # Create your views here.
 
 @method_decorator(login_required, name='dispatch')
 class GraficosDetailView(TemplateView):
     template_name = "graficosPie/grafico1.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        fases = Fase.objects.all().order_by('id')
+        cantidad_fases = Fase.objects.count()
+        tiempos_programa = RegistroTiempo.objects.filter(id_programa__id_usuario__id=self.request.user.id)
+        defectos_programa = RegistroDefecto.objects.filter(id_programa__id_usuario__id=self.request.user.id)
+        datos, actual = matrizResumenTiempos(tiempos_programa, fases, cantidad_fases)
+        defectos_ingresados = defectos_en_fase(defectos_programa, fases, cantidad_fases)
+
+        context["datos"], context["total_actual"] = matrizResumenTiempos(tiempos_programa, fases, cantidad_fases)
+        context["defectos_ingresados"] = defectos_en_fase(defectos_programa, fases, cantidad_fases)
+        context["defectos_removidos"] = defectos_removidos_fase(defectos_programa, fases, cantidad_fases)
+
+        lista = []
+        for d in datos:
+            lista.append(d[4])
+        context["lista"] = lista
+
+        listaD = []
+        for a in defectos_ingresados:
+            listaD.append(a[4])
+        context["listaD"] = listaD
+
+        return context
+    
