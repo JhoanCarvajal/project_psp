e93b90ee3be89e6f03fb7b28c77f7ad78faf0a12
Michael <michass1425@gmail.com>
2020-07-31 19:49:09 -0500

    asdf

--- PspProject/graficos/urls.py
+++ PspProject/graficos/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import GraficosDetailView
 
 urlpatterns = [
-    path('estadisticas/<int:pk>', GraficosDetailView.as_view(), name='ver_estadisticas'),
+    path('estadisticas/', GraficosDetailView.as_view(), name='ver_estadisticas'),
 ]
